6fdaf7a578cc13318a48448f5fd2a8d10420dc4e
GSri30 <sri_3007@hotmail.com>
2023-05-14 00:58:57 +0530

    add: django-tests-user-app

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from users.models import CustomUser
 
-# Create your tests here.
+class CustomUserCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(first_name="Sri Harsha", last_name="G", email="ghant.sriharsha@gmail.com")
+    
+    def test_user_fetch(self):
+        user = CustomUser.objects.get(first_name = "Sri Harsha")
+        self.assertEqual(user.get_short_name(), "G")
+    
+    # def test_user_fetch_last_name(self):
+    #     user = CustomUser.objects.get(last_name = "G")
+    #     self.assertEqual(user.first_name, "Sri Harsha")
+    
+    # def test_multiple_user_fetch(self):
+    #     CustomUser.objects.create(first_name="Sri Harsha", last_name="G dash", email="ghant.sriharsha@gmail.com")
+        # user = CustomUser.objects.get(first_name = "Sri Harsha")
+        # self.assertEqual(user.last_name, "G")
+    
+    # def test_user_email_blank(self):
+    #     user = CustomUser.objects.get(first_name = "Sri Harsha")
